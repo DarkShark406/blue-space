e0d1c196dac82f1baad271c71dd78743366bf72e
lam-lethanh <lelam2002@gmail.com>
2023-04-28 15:54:19 +0700

    Add login popup

--- frontend/src/app/components/pages/login/login.component.ts
+++ frontend/src/app/components/pages/login/login.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+  clearClassIsInvalid(inputId: string) {
+    document.getElementById(inputId)?.classList.remove('is-invalid');
+  }
 }
