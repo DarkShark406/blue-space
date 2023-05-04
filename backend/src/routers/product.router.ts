8abf21a321912a9aab8e7789a8d90816de1833da
DarkShark406 <anpl20406@st.uel.edu.vn>
2023-05-04 17:24:20 +0700

    resolve conflict in product.ts

--- backend/src/routers/product.router.ts
+++ backend/src/routers/product.router.ts
@@ -6,38 +6,38 @@ const router = Router();
 
 // Import data to DB
 router.get("/seed", async (req, res) => {
-  const productCount = await ProductModel.countDocuments();
-  if (productCount > 0) {
-    res.send("Seed is already done!");
-    return;
-  }
-  await ProductModel.create(sample_products);
+	const productCount = await ProductModel.countDocuments();
+	if (productCount > 0) {
+		res.send("Seed is already done!");
+		return;
+	}
+	await ProductModel.create(sample_products);
 
-  res.send("Seed is done, Lam");
+	res.send("Seed is done, Lam");
 });
 
 router.get("/", async (req, res) => {
-  const data = await ProductModel.find();
-  res.send(data);
+	const data = await ProductModel.find();
+	res.send(data);
 });
 router.get("/search/:searchTerm", async (req, res) => {
-  const searchTerm = req.params.searchTerm;
-  const regex = new RegExp(searchTerm, "i");
-  const products = await ProductModel.find({ productName: { $regex: regex } });
-  res.send(products);
+	const searchTerm = req.params.searchTerm;
+	const regex = new RegExp(searchTerm, "i");
+	const products = await ProductModel.find({ productName: { $regex: regex } });
+	res.send(products);
 });
 
 router.get("/brand/:categoryId", async (req, res) => {
-  try {
-    const { categoryId } = req.params;
+	try {
+		const { categoryId } = req.params;
 
-    const brands = await ProductModel.distinct("productBrand", { categoryId });
+		const brands = await ProductModel.distinct("productBrand", { categoryId });
 
-    res.json(brands);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
-  }
+		res.json(brands);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Server error" });
+	}
 });
 
 export default router;
