0418205173f3348e1ed260a332bd2dcbcc2b3d7c
DarkShark406 <anpl20406@st.uel.edu.vn>
2023-05-04 17:25:33 +0700

    resolve conflict in app.component.html

--- frontend/src/app/components/partials/footer/footer.component.ts
+++ frontend/src/app/components/partials/footer/footer.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  expandList(idTitle: string) {
+    document.getElementById(idTitle)?.classList.toggle('active');
+  }
+}
