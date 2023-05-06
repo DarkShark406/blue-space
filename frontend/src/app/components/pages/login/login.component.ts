5ab066323efa7674d9ada1136884416d94140cc3
lam-lethanh <lelam2002@gmail.com>
2023-05-06 20:42:41 +0700

    Done Login Page & User Service

--- frontend/src/app/components/pages/login/login.component.ts
+++ frontend/src/app/components/pages/login/login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-
+import { IUserLogin } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  user: IUserLogin = new IUserLogin();
+  returnUrl = '';
+  message = '';
+  constructor(private userService: UserService, private location: Location) {}
+  ngOnInit() {}
+  login() {
+    this.userService
+      .login({
+        email: this.user.email,
+        password: this.user.password,
+      })
+      .subscribe(() => {
+        this.location.back();
+      });
+  }
+
   clearClassIsInvalid(inputId: string) {
     document.getElementById(inputId)?.classList.remove('is-invalid');
   }
