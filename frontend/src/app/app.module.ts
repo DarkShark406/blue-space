029c3ae7ec208e5961912d024ceac72b923ca6a3
DThKim <131367178+DThKim@users.noreply.github.com>
2023-05-07 23:29:41 +0700

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { Faq1Component } from './components/pages/faq1/faq1.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Faq2Component } from './components/pages/faq2/faq2.component';
 import { Faq3Component } from './components/pages/faq3/faq3.component';
+import { FAQ4Component } from './components/pages/faq4/faq4.component';
+import { FAQ41Component } from './components/pages/faq41/faq41.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Faq1Component,
     Faq2Component,
-    Faq3Component
+    Faq3Component,
+    FAQ4Component,
+    FAQ41Component,
   ],
   imports: [
     BrowserModule,
